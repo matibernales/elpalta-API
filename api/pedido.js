a3633ff8d98f58faa14c16ea408551156a46304e
matibernales <matiasbernales@yahoo.com>
2026-06-16 16:34:19 -0400

    Update pedido.js

--- api/pedido.js
+++ api/pedido.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -29,4 +29,4 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ ok: false, error: err.message });
   }
-}
+};
